723565003a629092682231c14e48ee02eb5cd1fe
Hubert Łępicki <hubert.lepicki@amberbit.com>
2021-11-04 10:39:47 +0100

    Add submit event handling to form field component

--- lib/spike/surface/form_field.ex
+++ lib/spike/surface/form_field.ex
@@ -4,13 +4,14 @@ defmodule Spike.Surface.FormField do
   prop(form_data, :struct, required: true)
   prop(key, :atom, required: true)
   prop(target, :any, required: false, default: nil)
+  prop(submit_event, :string, required: false, default: "submit")
 
   slot(default)
 
   @impl true
   def render(assigns) do
     ~F"""
-    <form phx-change="spike-form-event:set-value" phx-target={@target}>      
+    <form phx-change="spike-form-event:set-value" phx-target={@target} phx-submit={@submit_event}>      
       <input name="ref" type="hidden" value={@form_data.ref} />
       <input name="key" type="hidden" value={@key} />
       <#slot />
