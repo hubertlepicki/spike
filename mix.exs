9b0b6a2a3e194f70efe279344673fd6652ccefb2
Hubert Łępicki <hubert.lepicki@amberbit.com>
2023-08-29 13:48:05 +0200

    Version bump: v0.2.5

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Spike.MixProject do
     [
       app: :spike,
       description: @description,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
